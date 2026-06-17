1f370fafe08c327eb5d32901f64fb53256d6a6e8
akhileshkumarthakur <mumbai2025.py@gmail.com>
2026-06-17 07:06:39 +0000

    day 0

---
+++ data.js
@@ -0,0 +1,33 @@
+const students = [
+  {
+    name: "Rahul Sharma",
+    rollNo: 101,
+    totalMarks: 450,
+    contact: {
+      email: "rahul@example.com",
+      phone: "9876543210"
+    }
+  },
+
+  {
+    name: "Priya Patel",
+    rollNo: 102,
+    totalMarks: 470,
+    contact: {
+      email: "priya@example.com",
+      phone: "9876543211"
+    }
+  },
+
+  {
+    name: "Amit Kumar",
+    rollNo: 103,
+    totalMarks: 430,
+    contact: {
+      email: "amit@example.com",
+      phone: "9876543212"
+    }
+  }
+];
+
+export default students;
