1d01d56becd24849e78107988e8b6d6152330181
akhileshkumarthakur <mumbai2025.py@gmail.com>
2026-06-24 10:49:25 +0000

    day 5 commit

--- vite-project/src/components/product.jsx
+++ vite-project/src/components/product.jsx
@@ -1,69 +1,40 @@
 import React from "react";
-import './product.css'
-import cloudImage from './cloud-image.webp'
+import "./product.css";
+import cloudImage from "./cloud-image.webp";
+import data from "./products-data.js";
 
-function Product () {
+function Product() {
+  return (
+    <div className="products-container">
+      {data.map((product) => (
+        <div className="product-card" key={product.id}>
+          <img
+            className="product-image"
+            src={cloudImage}
+            alt={product.title}
+          />
 
-    const data = [
-        {
-            image: "this is image url",
-            title: "Wireless Headphones",
-            rating: "⭐⭐⭐⭐☆ (4.2)",
-            price: "$59.99"
+          <div className="product-title">
+            {product.title}
+          </div>
 
-        }
-    ]
+          <div className="product-rating">
+            {product.rating}
+          </div>
 
-    return (
-        <div className="products-container">
-            <div className="product-card">
-                <img
-                className="product-image"
-                src={cloudImage}
-                alt="Product Image"
-                />
+          <div className="product-price">
+            ${product.price}
+          </div>
 
-                <div className="product-title">
-                {data[0].title}
-                </div>
-
-                <div className="product-rating">
-                {data[0].rating}
-                </div>
-
-                <div className="product-price">
-                {data[0].price}
-                </div>
-
-                <div className="button-div"><button className="cart-button">Add to Cart</button></div>
-            </div>
-
-             <div className="product-card">
-                <img
-                className="product-image"
-                src={cloudImage}
-                alt="Product Image"
-                />
-
-                <div className="product-title">
-                {data[0].title}
-                </div>
-
-                <div className="product-rating">
-                {data[0].rating}
-                </div>
-
-                <div className="product-price">
-                {data[0].price}
-                </div>
-
-                <div className="button-div"><button className="cart-button">Add to Cart</button></div>
-            </div>
+          <div className="button-div">
+            <button className="cart-button">
+              Add to Cart
+            </button>
+          </div>
         </div>
-    )
+      ))}
+    </div>
+  );
 }
 
-
-
-
-export default Product
+export default Product;
